10d3be5103a765eba39a125d8f13fa3b9652823d
oleg_zalipskiy <oleg.zalipskiy@itomy.ch>
2020-09-03 16:52:15 +0300

    added dict, list and orderDict files

--- Python/dictionary.py
+++ Python/dictionary.py
@@ -1,21 +1,86 @@
-# назначаем значения
-configs = {
-    "browser": "Safari",
-    "app": "google site",
-    "test": "smoke",
-    "log": True
+# # назначаем значения                      Старый код с предыдущего курса.
+# configs = {
+#     "browser": "Safari",
+#     "app": "google site",
+#     "test": "smoke",
+#     "log": True
+# }
+
+# # выводим определенной значение
+# print(configs.get("browser"))
+
+# # Выводим ключ
+# for conf in configs:
+#     print(conf)
+
+# # выоводим значения
+# for conf in configs.values():
+#     print(conf)
+
+# if "browser" in configs:
+#     print("Exist")
+
+# Создаем словарь
+players = {
+    'Carlsen': 2842,
+    'Caruana': 2822,
+    'Mamedyrov': 2801,
+    'Ding': 2797,
+    'Giri': 2780
 }
 
-# выводим определенной значение
-print(configs.get("browser"))
+# players = dict(Carlsen=2842) - еще один вариант синтаксиса для сохдания списка
 
-# Выводим ключ
-for conf in configs:
-    print(conf)
+# Ищем елемент по ключу
+top1 = players['Carlsen']
+print(f"Top chess player's rating is {top1}")
 
-# выоводим значения
-for conf in configs.values():
-    print(conf)
+# Ищем елемент по ключу
+players.get('Carlsen')
 
-if "browser" in configs:
-    print("Exist")
+# Добавляем елемент
+players['So'] = 2780
+print(players)
+
+# Удаляем елемент по ключу
+del players['So']
+print(players)
+
+# Выводим только ключи
+keys = players.keys()
+print(keys)
+print(type(keys))
+
+# Конвертируем в список
+playersList = list(players.keys())
+print(playersList)
+print(type(playersList))
+
+# Сортировка
+print(sorted(players.keys()))
+
+# Проверка на True/False
+print('Carlsen' in players)
+print('Kramnik' not in players)
+
+# Выводим значения и конвертируем в список
+print(players.values())
+valuesList = list(players.values())
+print(valuesList)
+
+# цикл для вывода всех позиций из списка
+for key, value in players.items():
+    print(key, value)
+
+# Удалить по ключу (без аргумента удалится последний)
+players.pop('Giri')
+
+#Удалить перввый
+players.popitem()
+
+# Длина словаря
+len(players)
+
+# Добавить ключ если он будет не найден и установить дефолтное значение None
+players.setdefault('Karjakin')
+print(players)
---
+++ Python/lists.py
@@ -0,0 +1,48 @@
+int_list = [1, 2, 3,] # Список чисел
+mix_list = [1, 2.0, 'string'] # Смешанный список
+print(len(int_list)) # длинна списка
+print(int_list[0]) # Вывод позиции по индексу
+print(int_list[-1]) # Вывод позиции по индексу с конца
+print(int_list[1:]) # Вывод позиций начиная со 2
+
+names1 = ['John', 'Bob', 'Elias']
+names2 = ['Tracy', 'Mason', 'Billy']
+nameesCombined = names1+names2 # Конкастинация списков
+
+nameesCombined[1]='Willian' # Замена позиции по индексу
+
+nameesCombined.append('Tomas') # Добавление записи в список
+
+popped = nameesCombined.pop() # УДаление последнего елемента
+
+print(popped)
+
+popped1 = nameesCombined.pop(2) # Удаление елемента по индексу
+print(nameesCombined)
+
+letters = ['ab', 'ac', 'aa']
+letters.sort() # Сортировка списка
+print(letters)
+
+letters1 = ['abc', 'a', 'ab']
+letters1.sort(key=len) # Сортировка по длине
+print(letters1)
+
+numbers = [1, 2, 3,56,14,64,12,66,11,]
+print(numbers)
+numbers.sort()
+print(numbers)
+numbers.reverse() # Вывод списка наоборот
+print(numbers)
+numbers.sort(reverse=True) # Сортировка наоборот
+
+numbers.insert(1, 22) # Добавление значения после 2 елемента
+print(numbers)
+
+print(numbers.index(56)) # Вывести индекс числа 5
+print(numbers.count(3)) # Посичтать количество числа 3
+copy = numbers.copy() # Копия списка
+print(copy)
+
+numbers.clear() # Очистить список
+print(numbers) 
